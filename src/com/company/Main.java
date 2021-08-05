baabcdf441c69d317972030e9fb0f89b3e3145ce
Oleksii <oleksii.starodub@refinitiv.com>
2021-08-05 16:28:34 +0300

    Another last second commit on 4:28 PM.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,1 +1,1 @@
-package com.company;public class Main {    public static void main(String[] args) {	System.out.println("Another last first commit");    }}
+package com.company;public class Main {    public static void main(String[] args) {        System.out.println("Another last first commit dfsfsf");        System.out.println("Another last second commit created on 4:28 PM");    }}
