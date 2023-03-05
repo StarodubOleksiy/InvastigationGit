64a84309b7552651dc4db5d7ec0885d46106fa09
StarodubOleksiy <starodubaleks@gmail.com>
2023-03-05 20:39:23 +0200

    Second commit made today on sunday at 05 march of 2023 year

--- src/com/company/FourthClassCreatedOnSecondPage.java
+++ src/com/company/FourthClassCreatedOnSecondPage.java
@@ -1,1 +1,1 @@
-package com.company;public class FourthClassCreatedOnSecondPage {    public FourthClassCreatedOnSecondPage() {        System.out.println("Creating instance of fourth class which was created on second branch.");    }}
+package com.company;public class FourthClassCreatedOnSecondPage {    public FourthClassCreatedOnSecondPage() {        System.out.println("The second change made today on Sunday at 05 march of 2023 year");        System.out.println("Creating instance of fourth class which was created on second branch.");    }}
